54b8bd05d83648278c585a028d3b2233a579898a
temesgen-abebayehu <temesgensoftware21@gmail.com>
2026-07-30 22:27:25 +0300

    configure Prisma with Task schema and PostgreSQL

---
+++ backend/prisma/migrations/20260730191441_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateEnum
+CREATE TYPE "TaskStatus" AS ENUM ('TO_DO', 'IN_PROGRESS', 'DONE');
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "status" "TaskStatus" NOT NULL DEFAULT 'TO_DO',
+    "dueDate" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
